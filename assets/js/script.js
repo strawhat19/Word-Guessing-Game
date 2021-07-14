987d80ad6aaf1e459fd9089381f34c0dfd88aca8
Rakib Ahmed <rakib987@gmail.com>
2021-07-14 00:44:50 -0400

    Adding assets, changes to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,14 @@ var timeRemaining = timer.innerHTML;
 
 // Start Game on Button Click
 startButton.addEventListener("click", function(event) {
+    // Start Timer on Button Click
+    var countDownTimer = setInterval(function countDownFunction() {
+        // If the timer hits or goes below 0
+        if (timeRemaining <= 0) {
+            clearInterval(countDownTimer);
+        }
+        // Decrement the timer to countdownwards
+        timeRemaining -= 1;
+    }, 1000);
 
-    // Start Timer on Button Click // Run it in a loop
-    // for (var i = 0; timeRemaining > 0; i++) {
-        setInterval(timerCountDown,1000);
-        
-        function timerCountDown() {
-            var countDown = timeRemaining--;
-            timeRemaining.innerHTML = countDown;
-       }
-
-    // }
-    // return;
 })
